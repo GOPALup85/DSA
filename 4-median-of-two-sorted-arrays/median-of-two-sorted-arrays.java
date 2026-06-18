cd9d568fc487aa861d88fa1888ce4db0274b2a07
GOPAL SINGH <jaatgopal281308@gmail.com>
2026-06-18 22:46:03 +0530

    Time: 8 ms (13.99%) | Memory: 49.1 MB (14.90%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,21 +3,20 @@ class Solution {
         int len1 = nums1.length;
         int len2 = nums2.length;
         int len = len1+len2;
-        double median=0;
+        // double median=0;
         int merged_array[]= new int[len];
         for(int i=0;i<len1;i++){
             merged_array[i]= nums1[i];
-
         } 
         for(int i=0;i<len2;i++){
             merged_array[len1+i]= nums2[i];
         }
         Arrays.sort(merged_array);
         if(len%2==0){
-            median = (merged_array[len/2-1]+merged_array[len/2])/2.0;
+            return( (merged_array[len/2-1]+merged_array[len/2])/2.0);
         }else{
-            median= merged_array[len/2];
+            return( merged_array[len/2]);
         }
-        return median;
+        // return median;
     }
 }
