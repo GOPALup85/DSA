ee827c01d7719ebfbf7b3d54ae44080c7810ca7f
GOPAL SINGH <jaatgopal281308@gmail.com>
2026-06-22 22:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (10.87%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        double output =Math.pow(x,n);
-        return output;
+        long N =n;
+        if(N<0){
+            x = 1/x;
+            N = -N;
+        }
+        double answer=1;
+        while(N>0){
+            if(N%2==1){
+                answer*= x;
+            }
+            x*=x;
+            N /=2;
+        }
+        return answer;
     }
 }
