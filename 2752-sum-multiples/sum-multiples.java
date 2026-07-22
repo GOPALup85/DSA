af0254ccb8d7eff9c12a169089725497aa799184
GOPAL SINGH <jaatgopal281308@gmail.com>
2026-07-22 22:02:01 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (90.61%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -1,11 +1,18 @@
 class Solution {
+
     public int sumOfMultiples(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                sum += i;
-            }
-        }
-        return sum;
+
+        return sum(n, 3)
+             + sum(n, 5)
+             + sum(n, 7)
+             - sum(n, 15)
+             - sum(n, 21)
+             - sum(n, 35)
+             + sum(n, 105);
+    }
+
+    private int sum(int n, int k) {
+        int m = n / k;
+        return k * m * (m + 1) / 2;
     }
 }
